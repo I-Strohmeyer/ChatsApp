e1f506c2f462e62563a9543edacdb38a14b3b71f
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2022-07-31 14:12:12 +0200

    add gifted chat integration

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,89 @@
 import React from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
-    // display user name at the top of scren
+    // display user name at the top of screen
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name } || "Unknown");
+
+    // add static message
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hello ${name}, welcome to this group!`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#c9c9c9",
+          },
+        }}
+      />
+    );
   }
 
   render() {
     return (
       //set bgColor that user selected
-      <ScrollView
+      <View
         style={{
           flex: 1,
           backgroundColor: this.props.route.params.bgColor,
         }}
       >
-        <Text>Hello Chat!</Text>
-      </ScrollView>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
+      </View>
     );
   }
 }
